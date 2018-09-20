579963503a97d8caa5cb2298995b18b8fd30a08a
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-20 01:47:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ class Driver {
     return parseInt(totalYears);
   };
 }
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  };
+  
+}
