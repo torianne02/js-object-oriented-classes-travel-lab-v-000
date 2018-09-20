7cd4cc5efc7d9a7193137c9852fe7030960086ca
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-20 01:51:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Route {
   blocksTravelled() {
     let horizontalDistance = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal);
     let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical;
-  }
+    return (Math.abs(horizontalDistance) + Math.abs(verticalDistance));
+  };
 
 }
